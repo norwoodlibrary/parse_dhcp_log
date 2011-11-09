196194d401aa7ffc26360904f5ab1a0945850e63
Brian Samek <brian@norwoodlibrary.org>
2011-11-09 13:11:07 -0500

    Added total across all days.

--- parse_dhcp_log.py
+++ parse_dhcp_log.py
@@ -3,6 +3,7 @@ import re, sys
 def main(log_name):
     re_pattern = re.compile(r'^.{3},\s(.{11}).*(..:..:..:..:..:..)')
     result = {}
+    total = 0
 
     log = open(log_name)
     for line in log:
@@ -16,10 +17,15 @@ def main(log_name):
                 result[date] = set([mac_address])
 
     # This uses alphabetical sorting so will only work within a month.
+    # It also totals all results without regard to month.
     dates = result.keys()
     dates.sort()
     for date in dates:
-        print "%s: %s" % (date, len(result[date]))
+        day_subtotal = len(result[date])
+        total += day_subtotal
+        print "%s: %s" % (date, day_subtotal)
+    print "\nTotal = %d" % total
+
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
